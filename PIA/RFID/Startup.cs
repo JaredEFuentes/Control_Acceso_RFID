b3c4cf3db34d95d2940962801140c70628a29dfd
Alberto Sanchez <albertonatanael486@gmail.com>
2021-05-15 15:22:03 -0500

    Servidor Completo v3

--- PIA/RFID/Startup.cs
+++ PIA/RFID/Startup.cs
@@ -40,12 +40,6 @@ namespace RFID
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             //app.UseStaticFiles();
@@ -55,6 +49,8 @@ namespace RFID
             //}
 
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
